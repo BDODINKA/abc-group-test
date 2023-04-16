1f21e7ae0ba02072b0d96abc31043180064f76b6
DMITRY <bdodinka1992@gmail.com>
2023-04-16 19:07:44 +0300

    feat:add spinner

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,6 @@ import Button from '@/components/common/button/Button.vue'
 import ScrollBtn from '@/components/common/scrollBtn/ScrollBtn.vue'
 import Title from '@/components/common/title/Title.vue'
 import Wrapper from '@/components/common/wrapper/Wrapper.vue'
+import Spinner from '@/components/common/spinner/Spinner.vue'
 
-export default [Button, ScrollBtn, Title, Wrapper]
+export default [Button, ScrollBtn, Title, Wrapper, Spinner]
